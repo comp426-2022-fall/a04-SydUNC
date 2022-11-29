e3bf9cc9bf14383bb0ddf35571ee386056f300cd
SydUNC <sydneyg@live.unc.edu>
2022-11-29 16:43:46 -0500

    Changed 404 message to match

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/app/roll/', (req, res) => {
 	res.send(console.log(JSON.stringify(roll(side, die, rolls))));
 })
 app.use((req, res, next) => {
-  res.status(404).send("404 Not Found!")
+  res.status(404).send("404 NOT FOUND")
 })
 
 app.listen(port, () => {
